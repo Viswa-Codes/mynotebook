5cb94ac14abc81ba3c8ce301c922d6f4e50a2f54
Viswa-Codes <viswatejabbireddyprof@gmail.com>
2025-11-16 17:00:46 +0530

    storing data into DB using mongoose Model

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,8 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//Middleware to use req.body or anyincoming json data
+app.use(express.json());
 
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,10 +2,15 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 
-
-router.get('/', (req, res) => {
-    res.send('Hello from auth route');
+//To use req.body, we need to use express.json() middleware in index.js
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 });
+
+// ROUTE 1: Create a User using: POST "/api/auth/createuser". No login required
 
 
 module.exports = router
