2900d96871720008d5d21f1cfdc4a1b60f54f97e
Viswa-Codes <viswatejabbireddyprof@gmail.com>
2025-12-07 09:03:55 +0530

    Creating Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar } from './componenets/Navbar';
+import { Home } from './componenets/Home';
+import { About } from './componenets/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to My Notebook App</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/componenets/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+  
+export const About = () => {
+    return (
+        <div>
+            This is About
+        </div>
+    )
+}
