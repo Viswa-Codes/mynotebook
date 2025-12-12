8e5c76e3e6a2a9935074b55af84f7fc55636ac05
Viswa-Codes <viswatejabbireddyprof@gmail.com>
2025-12-12 09:29:17 +0530

    Adding AddNote Component

--- src/componenets/Home.js
+++ src/componenets/Home.js
@@ -4,12 +4,6 @@ export const Home = () => {
 
     return (
         <div>
-
-            <div className="container my-3">
-                <h2>Add a Note</h2>
-                <form></form>
-            </div>
-
            <Notes/>
         </div>
     )
--- src/componenets/Notes.js
+++ src/componenets/Notes.js
@@ -1,18 +1,22 @@
 import React, { useContext } from 'react';
  import NoteContext from '../context/notes/noteContext';
  import Noteitem from './Noteitem';
+import AddNote from './AddNote';
 
 const Notes = () => {
         const context = useContext(NoteContext);
-    const { notes, setNotes } = context;
+    const { notes,addNote} = context;
 
     return (
+        <>
+        <AddNote/>
          <div className="row my-3">
                 <h2>Your Notes</h2>
                 {notes.map((note) => {
                     return <Noteitem key={note._id} note={note}/>
                 })}
             </div>
+        </>
     )
 }   
 export default Notes;
