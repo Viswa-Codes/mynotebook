18bee78b6a25c98105e5c6d29b9fb7ff01644235
Viswa-Codes <viswatejabbireddyprof@gmail.com>
2025-12-12 10:49:36 +0530

    Delete Note Functionality

--- src/componenets/Noteitem.js
+++ src/componenets/Noteitem.js
@@ -11,7 +11,8 @@ const Noteitem = (props) => {
             <div className="card my-3">
                 <div className="card-body">
                     <div className="d-flex align-items-center">
-                        <h5 className="card-title">{note.title}</h5><i className="far fa-trash-alt mx-2"></i>
+                        <h5 className="card-title">{note.title}</h5>
+                        <i className="far fa-trash-alt mx-2" onClick={()=>{deleteNote(note._id)}}></i>
                         <i className="far fa-edit mx-2"></i>
                     </div>
                     <p className="card-text">{note.description} </p>
